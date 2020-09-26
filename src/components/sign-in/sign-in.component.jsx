1ec1dd7ebe9b2143809e6e763b821746a5f83453
roshin-India <roshinkrosh@gmail.com>
2020-09-26 16:31:50 +0530

    google sign in and firebase integration

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,6 +2,7 @@ import React from "react"
 import './sign-in.styles.scss'
 import CustomButton from "../custom-button/custom-button.component";
 import FormInput from "../form-input/form-input.component";
+import {signInWithGoogle} from '../../firebase/firebase.utils.js'
 class SignIn extends React.Component{
     constructor(){
         super();
@@ -26,10 +27,12 @@ class SignIn extends React.Component{
                 <span>Sign in with your email and password</span>
 
                 <form onSubmit={this.handleSubmit}>
-                    <FormInput name="email" type="email" value={this.state.email} label="Email" handleChange={this.handleChange} required/>
-                    <FormInput name="password" type="password" value={this.state.password} label="Password" handleChange={this.handleChange} required/>
-
-                    <CustomButton type="submit">Sign in</CustomButton>
+                    <FormInput name="email" type="email" value={this.state.email} label="Email" handleChange={this.handleChange} />
+                    <FormInput name="password" type="password" value={this.state.password} label="Password" handleChange={this.handleChange} />
+                    <div className="butttons">
+                        <CustomButton type="submit">Sign in</CustomButton>
+                        <CustomButton onClick={signInWithGoogle} isGoogleSignIn> Sign in with Google</CustomButton>
+                    </div>
                 </form>
             </div>
         )
