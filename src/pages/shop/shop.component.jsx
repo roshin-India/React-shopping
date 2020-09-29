0120123c2255af3eab09706b62e835c775c1816d
roshin-India <roshinkrosh@gmail.com>
2020-09-29 13:37:37 +0530

    change shop data and directory home data to state and change class component to function

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,22 +1,18 @@
 import React from 'react'
-import SHOP_DATA from  './shop.data'
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 import CollectionPreview from '../../components/collection-preview/collection-preview.component'
-class ShopPage extends React.Component{
-    constructor(props){
-        super(props);
-        this.state={
-            collections:SHOP_DATA
-        }
-    }
-    render(){
-        const {collections} = this.state;
-        return (<div>
-            {
-                collections.map( ({id,...otherCollectionProp}) => (
-                    <CollectionPreview key={id} {...otherCollectionProp}/>
-                ))
-            }
-        </div>)
-    }
-}
-export default ShopPage;
+import { selectCollections } from '../../redux/shop/shop.selector';
+const ShopPage =({collections})=> (
+    <div>
+       {
+           collections.map( ({id,...otherCollectionProp}) => (
+               <CollectionPreview key={id} {...otherCollectionProp}/>
+           ))
+       }
+   </div>
+)
+const mapStateToProps = createStructuredSelector({
+    collections:selectCollections
+})
+export default connect(mapStateToProps)(ShopPage);
