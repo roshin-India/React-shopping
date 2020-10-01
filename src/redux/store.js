45164d9b893d576ac297655e649e2a4f16889931
roshin-India <roshinkrosh@gmail.com>
2020-10-01 22:49:10 +0530

    Handling assyncronous event using REDUX THUNK

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,9 +4,8 @@ import {persistStore} from 'redux-persist';
 import rootReducer from './root-reducer';
 import thunk from 'redux-thunk';
 const middleWares = [thunk];
-if(process.env.NODE_ENV === 'development'){
     middleWares.push(logger)
-}
+
 export const store = createStore(rootReducer,applyMiddleware(...middleWares));
 export const persistor = persistStore(store)
 export default {store,persistor};
